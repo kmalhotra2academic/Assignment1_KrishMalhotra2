fa61c8c0174a30fc51d687636e24bf11fadff03b
Krish <kmalhotra2@academic.rrc.ca>
2025-11-14 17:17:03 -0600

    add gui entry point to launch ClientLookupWindow

--- A04_main.py
+++ A04_main.py
@@ -1,18 +1,23 @@
 __author__ = "ACE Faculty"
 __version__ = "1.0.0"
-__credits__ = ""
+__credits__ = "Krish Malhotra"
 
 # REQUIREMENT - add import statements
+import sys
+
 from user_interface.client_lookup_window import ClientLookupWindow
 
 # GIVEN:
 from PySide6.QtWidgets import QApplication
 
 # GIVEN:
-if __name__ == "__main__":
-    import sys
-
+def main () -> None:
+    """
+    Main function to start the Client Lookup application.
+    """
     app = QApplication(sys.argv)
     mainWindow = ClientLookupWindow()
     mainWindow.show()
     sys.exit(app.exec())
+if __name__ == "__main__":
+    main()
